9697f0d81e26eadbbd5939e75a11cbcfa9c8c711
jabushpenka <daskovartem05@gmail.com>
2026-06-14 16:45:35 +0700

    почистил от лишнего

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-from classes.database import CyrilDB
-
 # запуск должен осуществляться в терминале командой:
 # uvicorn api:app
-
-def main():
-    db = CyrilDB()
-    q = db.group_check_hashkey('13', 'privet')
-    print(q)
-    return
-
-if __name__ == '__main__':
-    main()
